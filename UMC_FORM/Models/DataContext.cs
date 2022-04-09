6d312781322ce452fa235115bd7d556e04d6deaf
u34811 <u34811@UMC-C998>
2022-04-09 16:43:57 +0700

    - Tạo form mới - Accept

--- UMC_FORM/Models/DataContext.cs
+++ UMC_FORM/Models/DataContext.cs
@@ -4,6 +4,7 @@ namespace UMC_FORM.Models
     using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    using UMC_FORM.Models.GA;
 
     public partial class DataContext : DbContext
     {
@@ -21,7 +22,6 @@ namespace UMC_FORM.Models
         public virtual DbSet<Form_Role> Form_Roles { get; set; }
         public virtual DbSet<Form_Stations> Form_Stations { get; set; }
         public virtual DbSet<Form_User> Form_User { get; set; }
-        public virtual DbSet<LCA_F01> LCA_F01 { get; set; }
         public virtual DbSet<LCA_FILE> LCA_FILE { get; set; }
         public virtual DbSet<LCA_FORM_01> LCA_FORM_01 { get; set; }
         public virtual DbSet<LCA_PERMISSION> LCA_PERMISSION { get; set; }
@@ -29,6 +29,9 @@ namespace UMC_FORM.Models
         public virtual DbSet<PR_ACC_F06> PR_ACC_F06 { get; set; }
         public virtual DbSet<Form_Summary> Form_Summary { get; set; }
         public virtual DbSet<Form_Log> Form_Logs { get; set; }
+        public virtual DbSet<GA_LEAVE_FORM> GA_LEAVE_FORM { get; set; }
+        public virtual DbSet<GA_LEAVE_FORM_ITEM> GA_LEAVE_FORM_ITEM { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -48,13 +51,6 @@ namespace UMC_FORM.Models
                 .Property(e => e.DEPT)
                 .IsUnicode(false);
 
-            modelBuilder.Entity<LCA_F01>()
-                .Property(e => e.ID)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<LCA_F01>()
-                .Property(e => e.TICKET)
-                .IsUnicode(false);
 
             modelBuilder.Entity<PR_ACC_F06>()
                 .Property(e => e.ID)
---
+++ UMC_FORM/Models/GA/GA_LEAVE_FORM.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UMC_FORM.Models.GA
+{
+    public class GA_LEAVE_FORM
+    {
+        [Key]
+        [StringLength(50)]
+        public string ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string TICKET { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string DEPT { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CREATOR { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d/M/yyyy}")]
+        [Required]
+        public DateTime DATE_REGISTER { get; set; }
+
+        [Required]
+        public int NUMBER_REGISTER { get; set; }
+        public string FORM_NAME { get; set; }
+
+        [NotMapped]
+        public string DEPT_MANAGER { get; set; }
+
+        [NotMapped]
+        public string SHIFT_MANAGER { get; set; }
+
+        public int PROCEDURE_INDEX { get; set; }
+
+        public DateTime UPD_DATE { get; set; }
+
+        public int IS_SIGNATURE { get; set; }
+
+        public int ORDER_HISTORY { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string STATION_NAME { get; set; }
+        public string STATION_NO { get; set; }
+
+
+        public string SUBMIT_USER { get; set; }
+
+        public string TITLE { get; set; }
+        public string COMMENT { get; set; }
+
+        [NotMapped]
+        public List<GA_LEAVE_FORM_ITEM> GA_LEAVE_FORM_ITEMs { get; set; }
+    }
+}
